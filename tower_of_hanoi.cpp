8489cdfa1915ca0dde5512483805363104a830f9
gauraviitbhu2014 <118883439+gauraviitbhu2014@users.noreply.github.com>
2022-11-23 14:14:21 +0530

    Add files via upload

---
+++ tower_of_hanoi.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+void tower_of_hanoi(int n, char source, char dest, char helper){
+	if(n==0){
+		return;
+	}
+	tower_of_hanoi(n-1,source,helper,dest);
+	cout<<"move out from "<<source<<" to "<<dest<<endl;
+	tower_of_hanoi(n-1,helper,dest,source);
+	return;
+}
+int main(){
+	int n;
+	cin>>n;
+	tower_of_hanoi(n,'A','C','B');
+	return 0;
+}
